49c9fb2d37490fd99136712365fea3f8d882845b
GuilhermeAlvesVR <guilherme.s.alves96@gmail.com>
2026-03-03 20:40:42 -0300

    classe cliente com atributos e propriedades (nome e cpf)

---
+++ ConceitosPOO/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceitosPOO;
+
+Console.WriteLine("Hello, World!");
+Cliente cliente;
+cliente = new Cliente();
+cliente.Nome = "Seu nome";
+cliente.Cpf = "12345678910";
+
+Console.WriteLine($"Nome do cliente:{cliente.Nome}");
+Console.WriteLine($"Nome do cliente:{cliente.Cpf}");
+
+
